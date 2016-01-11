ae07f45a1a0533968cc84e58748960885d6ac2a8
Tim Snyder <snyder.tim@gmail.com>
2016-01-11 12:36:48 -0600

    rstudio recipe for tsnyder/rstudio-0.99.849-1
    NOTE: this was used to build on Centos6 and utilizes yum to install some prerequisites for
    building rstudio.  The recipe should be improved but this is what I started with

--- rstudio/build.sh
+++ rstudio/build.sh
@@ -1,19 +1,10 @@
-# Install gin, gwt, junitjar, selenium, and ChromeDriver for selenium
-# (https://github.com/rstudio/rstudio/blob/master/dependencies/common/install-gwt)
+# don't install boost using rstudio deps script, it is installed from conda
+perl -i.orig -pe '/install-boost/ and s/^/#/' dependencies/common/install-common
 
-BUILD_TOOLS_URL=https://s3.amazonaws.com/rstudio-buildtools/
 
-curl -L $BUILD_TOOLS_URL/gin-1.5.zip > gin-1.5.zip
-mkdir -p src/gwt/lib/gin/1.5
-unzip -qd gin-1.5.zip src/gwt/lib/gin/1.5
-
-curl -L $BUILD_TOOLS_URL/gwt-2.7.0.zip > gwt-2.7.0.zip
-mkdir -p src/gwt/lib/gwt/2.7.0
-unzip -qd gwt-2.7.0.zip src/gwt/lib/gwt/2.7.0
-
-curl -L $BUILD_TOOLS_URL/junit-4.9b3.jar > junit-4.9b3.jar
-mv junit-4.9b3.jar src/gwt/lib/junit-4.9b3.jar
-
+pushd dependencies/linux
+./install-dependencies-yum --exclude-qt-sdk
+popd
 
 
 mkdir build
@@ -24,6 +15,7 @@ export BOOST_ROOT=$PREFIX
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
       -DRSTUDIO_TARGET=Desktop \
       -DCMAKE_BUILD_TYPE=Release \
+      -DQT_QMAKE_EXECUTABLE=$PREFIX/lib/qt5/bin/qmake \
       ..
 
 make install
