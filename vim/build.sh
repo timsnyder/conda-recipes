31943a8bee7192d72cdf1272b23ecacaf3d43fb3
Tim Snyder <t.snyder@samsung.com>
2016-02-12 11:34:53 -0600

    recipe used for tsnyder/vim/7.4.1303/linux-64/vim-7.4.1303-py27pl5.18.2_0.tar.bz2 on anaconda

--- vim/build.sh
+++ vim/build.sh
@@ -1,33 +1,19 @@
 #!/bin/sh
 
-VF=$RECIPE_DIR/vimfiles
-if [ ! -d $VF ]; then
-    echo Hint: git clone https://github.com/tpn/vimfiles in recipe dir.
-fi
+# features=big required for multibyte char editing support
 
 ./configure --prefix=$PREFIX    \
-            --without-x         \
-            --without-gnome     \
-            --without-tclsh     \
-            --without-local-dir \
-            --enable-gui=no     \
+            --enable-gui=gtk2 \
             --enable-cscope     \
-            --enable-pythoninterp=yes
+            --enable-pythoninterp=yes \
+            --enable-perlinterp=yes \
+            --with-features=big \
+            --enable-fail-if-missing
+
 
 make
 make install
 
-VIM=$PREFIX/vim
-VIM74=$VIM/vim74
 
-mkdir -p $VIM
-mkdir -p $VIM74
-
-if [ -d $VF ]; then
-    cp -r $VF/doc $VIM74
-    cp -r $VF/colors $VIM74
-    cp -r $VF/plugin $VIM74
-    cp -r $VF/nerdtree_plugin $VIM74
-fi
 
 # vim:set ts=8 sw=4 sts=4 tw=78 et:
